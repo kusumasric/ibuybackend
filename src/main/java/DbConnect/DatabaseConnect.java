41193c2ed0fc156255d06d1da42a603a3ebf0eb4
kusumasric <kusuma.chunduru@gmail.com>
2018-04-13 16:20:40 -0700

    adding code for post request

--- src/main/java/DbConnect/DatabaseConnect.java
+++ src/main/java/DbConnect/DatabaseConnect.java
@@ -17,9 +17,9 @@ public class DatabaseConnect {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			con = DriverManager.getConnection("");
+			con = DriverManager.getConnection("jdbc:mysql://ibuy-mysql-instance.c0ke3suclupo.us-east-2.rds.amazonaws.com/ibuydb", "root", "root123*");
 
-
+			
 		} catch (SQLException exp) {
 			System.out.println("Exception  is :" + exp);
 		}
